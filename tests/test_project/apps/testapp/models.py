10b571c6362f3f5485de27745afbfd938d3a02b8
Charpentier Johan <cyberj@arcagenis.org>
2010-03-03 20:17:04 +0100

    Add default value and bound forms

--- tests/test_project/apps/testapp/models.py
+++ tests/test_project/apps/testapp/models.py
@@ -7,7 +7,7 @@ class Author(models.Model):
             ('MRS', _('Mrs.')),
             ('MS', _('Ms.')),
     )
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, default="Platon")
     title = models.CharField(max_length=3, choices=TITLE_CHOICES)
     birth_date = models.DateField(blank=True, null=True)
 
--- tests/test_project/apps/testapp/tests.py
+++ tests/test_project/apps/testapp/tests.py
@@ -8,7 +8,6 @@ from test_project.apps.testapp.forms import ContactForm, AuthorForm, WhatamessFo
 class SimpleTestCase(TestCase):
     def testFormbasic(self):
         cf = ContactForm()
-        from extjs.utils import ExtJSONEncoder
         expct = {"items":[
             {'fieldLabel': 'subject', 'xtype': 'textfield', 'fieldHidden': False, 'name': 'subject', 'header': 'subject', 'helpText': '', 'maxLength': 100, 'allowBlank': True},
             {'fieldLabel': 'message', 'xtype': 'textfield', 'fieldHidden': False, 'value': 'pony', 'name': 'message', 'header': 'message', 'helpText': '', 'allowBlank': True},
@@ -16,14 +15,20 @@ class SimpleTestCase(TestCase):
             {'fieldLabel': 'cc_myself', 'xtype': 'checkbox', 'fieldHidden': False, 'value': False, 'name': 'cc_myself', 'header': 'cc_myself', 'helpText': '', 'allowBlank': False},
         ]}
         self.assertEqual(expct, simplejson.loads(cf.as_extjs()))
+        cf = ContactForm({'subject':'PONY'})
+        expct["items"][0]["value"] = "PONY"
+        self.assertEqual(expct, simplejson.loads(cf.as_extjs()))
 
     def testModelFormbasic(self):
         cf = AuthorForm()
         expct = {"items":[
-            {"fieldLabel": "name", "xtype": "textfield", "fieldHidden": False, "header": "name", "allowBlank": True, "helpText": "", "maxLength": 100, "name": "name"},
+            {"fieldLabel": "name", "xtype": "textfield", "fieldHidden": False, "header": "name", "allowBlank": True, "helpText": "", "maxLength": 100, "name": "name", "value": "Platon"},
             {"xtype": "combo", "fieldLabel": "title", "name": "title", "header": "title", "fieldHidden": False, "value": "", "width": 150, "allowBlank": True, "helpText": "", "mode": "local", "store": [["", "---------"], ["MR", "Mr."], ["MRS", "Mrs."], ["MS", "Ms."]], "listWidth": "auto"},
             {"fieldLabel": "birth_date", "allowBlank": False, "fieldHidden": False, "name": "birth_date", "header": "birth_date", "helpText": "", "xtype": "datefield"}
             ]}
+        self.assertEqual(expct, simplejson.loads(cf.as_extjs()))
+        cf = AuthorForm({"name":"PONNY"})
+        expct["items"][0]["value"] = "PONNY"
         self.assertEqual(expct, simplejson.loads(cf.as_extjs()))
 
     def testModelFormcomplex(self):
